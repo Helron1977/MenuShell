5d5f82506243730f24a90c6cb4d936dbbf316a77
Roland Sainty <47913777+Helron1977@users.noreply.github.com>
2024-06-19 23:28:59 +0200

    Update menu.sh
    fonctionne avec xmessage en utilisant le x11 fowarding
    Nucléarisation du code

--- menu.sh
+++ menu.sh
@@ -1,168 +1,153 @@
 #!/bin/bash
 
-# Fonction pour vérifier l'existence d'un répertoire
+# Function to check if a directory exists
 check_directory_exists() {
     local directory="$1"
     if [ ! -d "$directory" ]; then
-        xmessage -center "Le répertoire $directory n'existe pas."
+        xmessage -center "The directory $directory does not exist."
         return 1
     fi
 }
 
-# Fonction pour lire le fichier de configuration et extraire les options de menu
+# Function to read the configuration file and extract menu options
 read_config_file() {
     local config_file="$1"
     local options=()
-
     while IFS=: read -r libelle action; do
         if [[ $libelle == \#* || -z $libelle ]]; then
             continue
         fi
         options+=("$libelle:$action")
     done < "$config_file"
-
-    # Joindre les options en une chaîne de caractères séparée par des nouvelles lignes
     local joined_options
     joined_options=$(printf "%s\n" "${options[@]}")
-
     echo "$joined_options"
 }
 
-# Fonction pour générer un fichier de configuration de menu à partir d'un répertoire
 generate_menu_config() {
     local directory="$1"
     local parent_menu_file="$2"
     local config_file="${directory##*/}.conf"
     check_directory_exists "$directory" || return 1
-    echo "# Menu généré à partir des sous-dossiers, fichiers .log et .zip de $directory" > "$config_file"
-
-    # Check if there are any subdirectories
-    if [ "$(find "$directory" -mindepth 1 -type d | wc -l)" -gt 0 ]; then
-        for subdir in "$directory"/*/; do
+    echo "# Menu generated from subdirectories, .log and .zip files of $directory" > "$config_file"
+    local subdirs
+    subdirs=$(find "$directory" -mindepth 1 -type d)
+    if [ -n "$subdirs" ]; then
+        for subdir in $subdirs; do
             subdir=$(basename "$subdir")
-            echo "Lister $subdir:Lister $directory/$subdir" >> "$config_file"
+            echo "List $subdir:List $directory/$subdir" >> "$config_file"
         done
     fi
-
-    for file in "$directory"/*.{log,zip}; do
-        if [ -f "$file" ]; then
-            file=$(basename "$file")
-            echo "Afficher $file:Afficher $directory/$file" >> "$config_file"
-            echo "Editer $file:Editer $directory/$file" >> "$config_file"
-        fi
+    local files
+    files=$(find "$directory" -maxdepth 1 -type f \( -name "*.log" -o -name "*.zip" \))
+    for file in $files; do
+        file=$(basename "$file")
+        echo "Display $file:Display $directory/$file" >> "$config_file"
+        echo "Edit $file:Edit $directory/$file" >> "$config_file"
     done
-
-    if [ "$parent_menu_file" != "menu.conf" ]; then
-        echo "Retour au menu précédent:$parent_menu_file" >> "$config_file"
-    fi
-    echo "Définir le répertoire actuel comme source:source=$directory" >> "$config_file"
-    echo "Définir le répertoire actuel comme cible:cible=$directory" >> "$config_file"
-    echo "Copier les fichiers du répertoire source vers le répertoire cible:cp \$source \$cible" >> "$config_file"
+    echo "Return to previous menu:$parent_menu_file" >> "$config_file"
+    echo "Set current directory as source:source=$directory" >> "$config_file"
+    echo "Set current directory as target:target=$directory" >> "$config_file"
+    echo "Copy files from source directory to target directory:cp \$source \$target" >> "$config_file"
 }
 
-# Fonction pour afficher un message avec xmessage et capturer la réponse de l'utilisateur
+# Function to display a message with xmessage and capture the user's response
 show_message_with_buttons() {
     local prompt="$1"
     shift
     local button_labels=("$@")
     local buttons=""
-
-    # Ajouter les boutons pour chaque option du menu
     for (( index = 0; index < ${#button_labels[@]}; index++ )); do
         buttons+="$(($index + 1)):${button_labels[$index]},"
     done
-
-    # Ajouter explicitement le bouton "Quitter" avec l'indice approprié
-    buttons+="X:Quitter"
-
-    # Afficher le message avec xmessage en position (0, 0)
+    buttons+="X:Quit"
     response=$(xmessage -geometry +0+0 -buttons "$buttons" -print "$(printf "$prompt")")
     echo "$response"
 }
 
-# Fonction pour afficher le menu dans xmessage
+# Function to handle arguments in an action
+handle_arguments() {
+    local action=$1
+    # Extract all occurrences of '%' followed by a number
+    local arg_indices=($(echo "$action" | grep -oP '%\d+' | grep -oP '\d+'))
+    # Prompt the user to enter a value for each argument
+    for index in "${arg_indices[@]}"; do
+        read -p "Enter a value for argument $index: " arg_value
+        # Replace the '%' followed by the index with the user's input
+        action=${action//%$index/$arg_value}
+    done
+    echo "$action"
+}
+
+# Function to evaluate an action
+eval_action() {
+    local action=$1
+    # Check if the action contains a '%' character
+    if [[ $action == *"%"* ]]; then
+        action=$(handle_arguments "$action")
+    fi
+    echo "Executing $action in terminal..."
+    eval "$action"
+    echo "========================================"
+}
+
+
+
+# Function to display the menu in xmessage
 display_menu() {
-    local menu_file=$1
-    local parent_menu_file=$2
-    # Lire les options à partir du fichier de configuration
+    local menu_file="$1"
+    local parent_menu_file="$2"
     local options_str
     options_str=$(read_config_file "$menu_file")
-    # Convertir la chaîne d'options en tableau
     IFS=$'\n' read -r -d '' -a options <<< "$options_str"
     local prompt=""
     local button_labels=()
-    # Lire le contenu du fichier entete.txt
-    entete_content=$(<entete.txt)
-    # Construire le prompt et les libellés de boutons pour chaque option du menu
+    local entete_content=$(<entete.txt)
     for i in "${!options[@]}"; do
         IFS=: read -r libelle action <<< "${options[$i]}"
         prompt+="$((i+1)). $libelle\n"
         button_labels+=("$libelle")
     done
-    # Ajouter l'en-tête au prompt
     prompt="$entete_content\n$prompt"
-    # Afficher le menu avec xmessage
     local choice_index
     choice_index=$(show_message_with_buttons "$prompt" "${button_labels[@]}")
     if [[ -n "$choice_index" ]]; then
-        # Vérifier si l'utilisateur a choisi "Quitter" en appuyant sur 'q'
         if [[ "$choice_index" == "X" ]]; then
             exit
         fi
-        local choice=$((choice_index))
+        local choice=$choice_index
         local selected_option="${options[$((choice-1))]}"
         IFS=: read -r libelle action <<< "$selected_option"
-        handle_action "$action" "$menu_file" "$parent_menu_file"
+        case $action in
+            "List "*)
+                folder_to_list="${action#List }"
+                config_file="${folder_to_list##*/}.conf"
+                generate_menu_config "$folder_to_list" "$menu_file"
+                display_menu "$config_file" "$menu_file"
+                ;;
+            "Display "*)
+                file_to_display="${action#Display }"
+                less "$file_to_display"
+                display_menu "$menu_file" "$parent_menu_file"
+                ;;
+            "Edit "*)
+                file_to_edit="${action#Edit }"
+                vi "$file_to_edit"
+                display_menu "$menu_file" "$parent_menu_file"
+                ;;
+            *.conf)
+                display_menu "$action" "$menu_file"
+                ;;
+            *)
+                eval_action "$action"
+                display_menu "$menu_file" "$parent_menu_file"
+                ;;
+        esac
     else
-        echo "Option invalide. Veuillez entrer un numéro valide."
+        echo "Invalid option. Please enter a valid number."
     fi
 }
 
-# Fonction pour gérer l'action sélectionnée
-handle_action() {
-    local action=$1
-    local menu_file=$2
-    local parent_menu_file=$3
-    # Check if the action contains a '%' character
-    if [[ $action == *"%"* ]]; then
-        # Extract all occurrences of '%' followed by a number
-        arg_indices=($(echo "$action" | grep -oP '%\d+' | grep -oP '\d+'))
-        # Prompt the user to enter a value for each argument
-        for index in "${arg_indices[@]}"; do
-            read -p "Enter a value for argument $index: " arg_value
-            # Replace the '%' followed by the index with the user's input
-            action=${action//%$index/$arg_value}
-        done
-    fi
-    case $action in
-        "Lister "*)
-            folder_to_list="${action#Lister }"
-            config_file="${folder_to_list##*/}.conf"
-            generate_menu_config "$folder_to_list" "$menu_file"
-            display_menu "$config_file" "$menu_file"
-            ;;
-        "Afficher "*)
-            file_to_display="${action#Afficher }"
-            less "$file_to_display"
-            display_menu "$menu_file" "$parent_menu_file"
-            ;;
-        "Editer "*)
-            file_to_edit="${action#Editer }"
-            vi "$file_to_edit"
-            display_menu "$menu_file" "$parent_menu_file"
-            ;;
-        *.conf)
-            display_menu "$action" "$menu_file"
-            ;;
-        *)
-            echo "Exécution de $action dans le terminal..."
-            eval "$action"
-            echo "========================================"
-            display_menu "$menu_file" "$parent_menu_file"
-            ;;
-    esac
-}
-
-# Exemple d'utilisation : afficher un menu à partir du fichier de configuration "menu.conf"
+# Start the script
 clear
 display_menu "menu.conf"
